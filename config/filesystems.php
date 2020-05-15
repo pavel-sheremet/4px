2d2b004c46bf9238f67c7c1ed6828bf4f89d265d
Pavel Sheremet <pavel-sheremet-dev@yandex.ru>
2020-05-15 16:58:53 +0300

    set logo disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,13 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'images' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/logo'),
+            'url' => env('APP_URL') . '/storage/logo',
+            'visibility' => 'public'
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
